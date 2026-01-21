a6b162809ee719b38c78789e547d043a54415cb8
Aman Das <amandash0088@gmail.com>
2026-01-21 09:38:28 +0545

    Update Vite config to use environment variable for API key

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   define: {
-    'process.env': {
-      API_KEY: "AIzaSyDu8N5o9rYA0P8jIpWPibSCetU5YAQneuU"
-    }
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 });
